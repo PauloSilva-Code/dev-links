93fb4ffc9f9529a65da6cec6ded568339675e72a
Paulo Silva <paulowrsilva@gmail.com>
2023-06-19 18:18:18 -0300

    :tada: initial commit

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    /*if(html.classList.contains('light')) {
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    }*/
+    html.classList.toggle("light")
+    
+     
+    //pegar a tag img
+    const img = document.querySelector("#profile img")
+   
+    //subtituir a imagem
+    if(html.classList.contains('light')) {
+
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./src/img/avatar-light.png")
+
+    } else {
+    //se tiver light mode, manter a imgagem normal
+    img.setAttribute("src", "./src/img/avatar.png")  
+
+    }
+}
+
